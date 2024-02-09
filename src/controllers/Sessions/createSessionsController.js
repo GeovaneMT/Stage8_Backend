5abff9c5fa42a3104e8cc28e48594fc5e7ac0e25
GeovaneMT <gmtgeovane@gmail.com>
2024-02-09 11:48:02 -0300

    hotfix

--- src/controllers/Sessions/createSessionsController.js
+++ src/controllers/Sessions/createSessionsController.js
@@ -3,6 +3,8 @@ const AppError = require("../../utils/AppError")
 const authConfig = require("../../configs/auth")
 const { sign } = require("jsonwebtoken")
 const { compare } = require("bcryptjs")
+const { validateEmail } = require("../assets/userValidation")
+
 
 async function createSessionController(request, response) {
   console.log("Logging in ...")
@@ -10,24 +12,32 @@ async function createSessionController(request, response) {
   const { email, password } = request.body
   console.log("Received request from body")
 
+  // Check email format
+  console.log("validating email format")
+  if (!email || !validateEmail(email)) {
+    console.log("error: Invalid email format")
+    throw new AppError("Invalid email format.", 400)
+  } else {
+    console.log("Email format is valid")
+  }
+
   // Fetching user from the database
   const user = await knex("users").where({ email }).first()
+  console.log("trying to fetch user from database...")
+
+  // Comparing passwords
+  const passwordMatch = await compare(password, user.password)
+
+  console.log("validating inputs")
 
   // Handling case where user doesn't exist
-  if (!user) {
+  if (!user || !passwordMatch) {
     console.log("E-mail and/or password incorrect")
     throw new AppError("E-mail and/or password incorrect.", 401)
   } else {
     console.log(
       `User fetched: id: ${user.id}, name: ${user.name}, email: ${user.email}`
     )
-  }
-
-  // Comparing passwords
-  const passwordMatch = await compare(password, user.password)
-  if (!passwordMatch) {
-    console.log("E-mail and/or password incorrect")
-    throw new AppError("E-mail and/or password incorrect.", 401)
   }
 
   // Creating JWT token for authentication
