8c2115371df51c580ece3e2315fdaadfbd9d9075
GeovaneMT <gmtgeovane@gmail.com>
2024-02-09 16:03:49 -0300

    hotfix

--- src/server.js
+++ src/server.js
@@ -1,29 +1,35 @@
 require("express-async-errors")
 
-const PORT = 3333
-
+const migrationsRun = require("./database/sqLite/migrations")
 const errorHandler = require("./utils/errorMiddleware")
 const uploadConfig = require("./configs/upload")
 
-const migrationsRun = require("./database/sqLite/migrations")
-
 const cors = require("cors")
-
 const express = require("express")
-
 const routes = require("./routes")
 
-const app = express()
 
+// Run database migrations
 migrationsRun()
 
-app.use(express.json())
-app.use(routes)
+// Initialize Express application
+const app = express()
 app.use(cors())
+app.use(express.json())
 
+// Serve static files
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+
+// Set up routes
+app.use(routes)
+
+// Error handling middleware
 app.use(errorHandler)
 
+// Set the port number
+const PORT = 3333
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
