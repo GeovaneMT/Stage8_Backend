90a616d839e07bfe8774babd13f1fab3133a4663
GeovaneMT <gmtgeovane@gmail.com>
2024-03-02 15:42:25 -0300

    rollback

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,7 +8,7 @@ const DeleteNotesController = require("./notes/DeleteNotesController")
 
 const indexTagController = require("./tags/indexTagController")
 
-const CreateSessionsController = require("./sessions/createSessionsController/index.js")
+const CreateSessionsController = require("./sessions/createSessionsController")
 
 const UpdateAvatarController = require("./avatar/UpdateAvatarController")
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 require("dotenv/config")
 require("express-async-errors")
 
-const migrationsRun = require("./dataBase/sqLite/migrations/index.js")
-const errorHandler = require("./utils/errorMiddleware/index.js")
-const uploadConfig = require("./configs/upload/index.js")
+const migrationsRun = require("./dataBase/sqLite/migrations")
+const errorHandler = require("./utils/errorMiddleware")
+const uploadConfig = require("./configs/upload")
 
 const cors = require("cors")
 const express = require("express")
