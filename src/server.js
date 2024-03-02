85794f7e263276e241f3bfe4a390f1a3d2473339
GeovaneMT <gmtgeovane@gmail.com>
2024-03-02 15:34:41 -0300

    server fix

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv/config")
 require("express-async-errors")
 
-const migrationsRun = require("./database/sqLite/migrations")
+const migrationsRun = require("./dataBase/sqLite/migrations")
 const errorHandler = require("./utils/errorMiddleware")
 const uploadConfig = require("./configs/upload")
 
