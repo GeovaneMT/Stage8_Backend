40f33a5f439fb6a618a7d7ae7085c9b4cd984e76
GeovaneMT <gmtgeovane@gmail.com>
2024-03-04 10:55:58 -0300

    knex production env

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,14 @@
 const path = require("path")
 
 module.exports = {
-
   development: {
     client: "sqlite3",
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
-
     pool: {
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
     },
-
     migrations: {
       directory: path.resolve(
         __dirname,
@@ -21,8 +18,32 @@ module.exports = {
         "migrations"
       ),
     },
-
     useNullAsDefault: true,
-
+  },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(
+        __dirname,
+        "path",
+        "to",
+        "production",
+        "database.db"
+      ),
+      // Replace "path/to/production" with the actual path to your production database file
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+    useNullAsDefault: true,
   },
 }
